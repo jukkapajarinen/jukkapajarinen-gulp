4938c4e633fbbfa329a85cda62c8247b7aefc8c0
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2021-08-28 02:03:50 +0300

    Updates to gulfile watch features.

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,17 @@ const path = require("path");
 /**********************************************************************/
 const postcssConfig = [
   autoprefixer({
-    overrideBrowserslist: ["defaults"]
+    overrideBrowserslist: ["defaults"],
   }),
   cssnano({
-    preset: ["default", { 
-      discardComments: { removeAll: true },
-      normalizeUrl: false
-    }]
-  })
+    preset: [
+      "default",
+      {
+        discardComments: { removeAll: true },
+        normalizeUrl: false,
+      },
+    ],
+  }),
 ];
 
 /**********************************************************************/
@@ -31,43 +34,55 @@ const webpackConfig = {
   mode: "production",
   entry: { main: "./scripts/main.js" },
   output: { path: path.resolve(__dirname, "./_build"), filename: "build.js" },
-  plugins: [ new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }) ],
+  plugins: [new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })],
   optimization: {
     minimize: true,
-    minimizer: [ new terserPlugin({
-      terserOptions: { format: { comments: false } },
-      extractComments: false,
-    }) ],
+    minimizer: [
+      new terserPlugin({
+        terserOptions: { format: { comments: false } },
+        extractComments: false,
+      }),
+    ],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: "/node_modules/",
-        use: [{ loader: "babel-loader", options: { presets: ["@babel/preset-env"] } }]
-      }
-    ]
-  }
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env"] },
+          },
+        ],
+      },
+    ],
+  },
 };
 
 /**********************************************************************/
 /* Gulp tasks for bundling css & js and copying static files
 /**********************************************************************/
 const fonts = () => gulp.src("./fonts/**/*").pipe(gulp.dest("./_build/fonts"));
-const icons = () => gulp.src("./node_modules/@fortawesome/fontawesome-free/webfonts/*.ttf")
-  .pipe(gulp.dest("./_build/fonts"));
-const images = () => gulp.src("./images/**/*").pipe(gulp.dest("./_build/images"));
+const icons = () =>
+  gulp
+    .src("./node_modules/@fortawesome/fontawesome-free/webfonts/*.ttf")
+    .pipe(gulp.dest("./_build/fonts"));
+const images = () =>
+  gulp.src("./images/**/*").pipe(gulp.dest("./_build/images"));
 const pages = () => gulp.src("./pages/index.html").pipe(gulp.dest("./_build"));
 const static = gulp.parallel(fonts, icons, images, pages);
-const styles = () => gulp.src("./styles/**/*.*css")
-  .pipe(gulpSass.sync().on("error", gulpSass.logError))
-  .pipe(plugins.concat("build.css"))
-  .pipe(plugins.cssimport())
-  .pipe(plugins.postcss(postcssConfig))
-  .pipe(gulp.dest("./_build"));
+const styles = () =>
+  gulp
+    .src("./styles/**/*.*css")
+    .pipe(gulpSass.sync().on("error", gulpSass.logError))
+    .pipe(plugins.concat("build.css"))
+    .pipe(plugins.cssimport())
+    .pipe(plugins.postcss(postcssConfig))
+    .pipe(gulp.dest("./_build"));
 const scripts = (callback) => {
   webpack(webpackConfig, (err, stats) => {
-    if(err) throw new gulpUtil.PluginError("webpack", err);
+    if (err) throw new gulpUtil.PluginError("webpack", err);
     gulpUtil.log("[webpack]", stats.toString({ colors: true }));
     callback();
   });
@@ -76,22 +91,27 @@ const scripts = (callback) => {
 /**********************************************************************/
 /* Gulp tasks for cleaning, building and serving
 /**********************************************************************/
-const clean = () => gulp.src("./_build", { read: false, allowEmpty: true })
-  .pipe(plugins.clean());
+const clean = () =>
+  gulp.src("./_build", { read: false, allowEmpty: true }).pipe(plugins.clean());
 const build = gulp.parallel(static, styles, scripts);
 const serve = () => {
   browserSync.init({
     server: { baseDir: "./_build" },
     port: 8080,
-    ui: false
+    ui: false,
   });
-  gulp.watch([
-    "./fonts/**/*.*",
-    "./images/**/*.*",
-    "./pages/**/*.*",
-    "./scripts/**/*.*",
-    "./styles/**/*.*"
-  ]).on("change", gulp.series(build, () => browserSync.reload()));
+  gulp.watch(["./fonts/**/*.*", "./images/**/*.*", "./pages/**/*.*"]).on(
+    "change",
+    gulp.series(static, () => browserSync.reload())
+  );
+  gulp.watch("./scripts/**/*.*").on(
+    "change",
+    gulp.series(scripts, () => browserSync.reload())
+  );
+  gulp.watch("./styles/**/*.*").on(
+    "change",
+    gulp.series(styles, () => browserSync.reload())
+  );
 };
 
 /**********************************************************************/
@@ -100,5 +120,5 @@ const serve = () => {
 module.exports = {
   clean: gulp.series(clean),
   build: gulp.series(clean, build),
-  serve: gulp.series(clean, build, serve)
+  serve: gulp.series(clean, build, serve),
 };
